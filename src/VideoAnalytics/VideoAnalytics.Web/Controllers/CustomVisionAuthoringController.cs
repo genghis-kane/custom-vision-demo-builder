61998bb05fbe76581ae57da553c77ed7f6160386
Jessica Kane <JessicaK@intergen.org.nz>
2020-09-13 22:31:22 +1200

    Add video processor library, extract frames to ClientApp folder on app service file system.

--- src/VideoAnalytics/VideoAnalytics.Web/Controllers/CustomVisionAuthoringController.cs
+++ src/VideoAnalytics/VideoAnalytics.Web/Controllers/CustomVisionAuthoringController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using VideoAnalytics.Web.Services.Interfaces;
 
 namespace VideoAnalytics.Web.Controllers
@@ -17,21 +17,32 @@ namespace VideoAnalytics.Web.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
+        private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly ICustomVisionAuthoringService _authoringService;
-        private readonly ILogger<CustomVisionAuthoringController> _logger;
+        private readonly IVideoFrameExtractionService _videoFrameExtractionService;
 
         public CustomVisionAuthoringController(
+            IWebHostEnvironment webHostEnvironment,
             ICustomVisionAuthoringService authoringService, 
-            ILogger<CustomVisionAuthoringController> logger)
+            IVideoFrameExtractionService videoFrameExtractionService)
         {
+            _webHostEnvironment = webHostEnvironment;
             _authoringService = authoringService;
-            _logger = logger;
+            _videoFrameExtractionService = videoFrameExtractionService;
         }
 
         [HttpGet]
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
             var response = await _authoringService.GetOrCreateProject();
+
+            // Ideally this would move to blob storage, but I'm not sure the ffmpeg library will handle it
+            string videoFile = $"{_webHostEnvironment.ContentRootPath}\\ClientApp\\videos\\training-video.mp4";
+            string saveImagesTo = $"{_webHostEnvironment.ContentRootPath}\\ClientApp\\frames";
+            int frameStep = 15;
+            int maxFrames = 50;
+
+            await _videoFrameExtractionService.SaveImageFrames(videoFile, saveImagesTo, frameStep, maxFrames);
 
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
---
+++ src/VideoAnalytics/VideoAnalytics.Web/Services/VideoFrameExtractionService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoAnalytics.Web.Services.Interfaces;
+using Xabe.FFmpeg;
+
+namespace VideoAnalytics.Web.Services
+{
+    public class VideoFrameExtractionService : IVideoFrameExtractionService
+    {
+        public async Task SaveImageFrames(string videoFile, string saveImagesTo, int frameStepSeconds, int maxFrames)
+        {
+            IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(videoFile);
+            var duration = (int)mediaInfo.VideoStreams.FirstOrDefault()?.Duration.TotalSeconds;
+
+            for (int i = 0; i < maxFrames; i++)
+            {
+                string output = Path.Combine(saveImagesTo, Guid.NewGuid() + ".png");
+                int fromSeconds = (i * frameStepSeconds);
+
+                if (fromSeconds > duration) break;
+
+                IConversion conversion = await FFmpeg.Conversions.FromSnippet.Snapshot(videoFile, output, TimeSpan.FromSeconds(fromSeconds));
+                IConversionResult result = await conversion.Start();
+            }
+        }
+    }
+}
