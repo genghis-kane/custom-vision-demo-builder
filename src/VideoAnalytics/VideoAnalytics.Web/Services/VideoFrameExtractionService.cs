87a6b631420a6cbad3df24b21b413565972bb81c
Jess Kane <kanje608@gmail.com>
2020-10-01 22:58:17 +1300

    Do not try to resave video frames if they already exist. (#4)
    Co-authored-by: Jessica Kane <Jessica.Kane@intergen.co.nz>

--- src/VideoAnalytics/VideoAnalytics.Web/Services/VideoFrameExtractionService.cs
+++ src/VideoAnalytics/VideoAnalytics.Web/Services/VideoFrameExtractionService.cs
@@ -14,16 +14,23 @@ namespace VideoAnalytics.Web.Services
         public async Task<VideoFrameExtractionResponse> SaveImageFrames(string videoFile, string saveImagesTo, int frameStepSeconds, int maxFrames)
         {
             var imageFilePaths = new List<string>();
+            var videoFileName = Path.GetFileNameWithoutExtension(videoFile);
 
             IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(videoFile);
             var duration = (int)mediaInfo.VideoStreams.First().Duration.TotalSeconds; //let's just assume not null
 
-            for (int i = 0; i < maxFrames; i++)
+            for (var i = 0; i < maxFrames; i++)
             {
-                string output = Path.Combine(saveImagesTo, Guid.NewGuid() + ".png");
-                int fromSeconds = (i * frameStepSeconds);
+                var fromSeconds = (i * frameStepSeconds);
+                var fileName = $"{videoFileName}-{fromSeconds}";
+                var output = Path.Combine(saveImagesTo, fileName + ".png");
 
                 if (fromSeconds > duration) break;
+                if (File.Exists(output))
+                {
+                    imageFilePaths.Add(output);
+                    continue;
+                }
 
                 IConversion conversion = await FFmpeg.Conversions.FromSnippet.Snapshot(videoFile, output, TimeSpan.FromSeconds(fromSeconds));
                 IConversionResult result = await conversion.Start();
