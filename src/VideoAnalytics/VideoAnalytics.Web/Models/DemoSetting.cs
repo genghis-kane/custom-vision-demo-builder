f4e60ce42b64d1b9b56761cf1010c95483245c2b
Jess Kane <kanje608@gmail.com>
2020-10-26 11:25:06 +1300

    Move demo URLs to app settings. (#14)
    Co-authored-by: Jessica Kane <Jessica.Kane@intergen.co.nz>

---
+++ src/VideoAnalytics/VideoAnalytics.Web/Models/DemoSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoAnalytics.Web.Models
+{
+    public class DemoSetting
+    {
+       public string Name { get; set; }
+       public string VideoUrl { get; set; }
+       public string PredictionsUrl { get; set; }
+    }
+}
