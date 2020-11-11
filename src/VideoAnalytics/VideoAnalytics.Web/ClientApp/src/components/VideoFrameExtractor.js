066571e85400c0813dccc7d00fc4fe5a2a4a3cd7
Jessica Kane <Jessica.Kane@intergen.co.nz>
2020-11-11 18:43:57 +1300

    Pull project selector out into separate component.

--- src/VideoAnalytics/VideoAnalytics.Web/ClientApp/src/components/VideoFrameExtractor.js
+++ src/VideoAnalytics/VideoAnalytics.Web/ClientApp/src/components/VideoFrameExtractor.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CreatableSelect from 'react-select/creatable';
 
+import { CustomVisionProjectSelector } from './form/CustomVisionProjectSelector';
 import { VideoImageFrameExtractor } from './form/VideoImageFrameExtractor';
 import { VideoImageFramePublisher } from './form/VideoImageFramePublisher';
 
@@ -13,18 +13,11 @@ export class VideoFrameExtractor extends Component {
     super(props);
     this.state = { 
       step: 0,
-      existingProjects: [],
-      selectedProject: '',
-      frames: [],
-      loading: false,
+      frames: []
     };
 
+    this.customVisionProjectSelectCallback = this.customVisionProjectSelectCallback.bind(this);
     this.extractVidoFramesCallback = this.extractVidoFramesCallback.bind(this);
-    this.getCustomVisionProjects = this.getCustomVisionProjects.bind(this);
-  }
-
-  componentDidMount() {
-    this.getCustomVisionProjects();
   }
 
   render() {
@@ -35,38 +28,18 @@ export class VideoFrameExtractor extends Component {
           <div class='row'>
             <p>This tool can be used to extract image frames out of an uploaded video. You will be given the option to select which extracted frames should be published up to your Custom Vision project. From there, they can be used to train your AI model.</p>
           </div>
-          <div style={{ display: this.state.loading ? 'block' : 'none' }}>
-            <div className='row'>
-              <p>Loading...</p>
-            </div>
-          </div>
-
-          <div style={{ display: (!this.state.loading && this.state.step === 0) ? 'block' : 'none' }}>
-            <div className='row'>
-              <div className='step-container'>
-                <form onSubmit={e => this.submitProject(e)}>
-                  <p className="step-title">Step 1: Select or create a custom vision project</p>
-                  <p>Choose an existing custom vision project, or type a name into the box to create a new custom vision project for your extracted image frames to be loaded into.</p>
-              
-                  <div className="form-group">
-                    <label for='projectName'>Project name: </label>
-                    <CreatableSelect
-                      id='projectName'
-                      name='projectName'
-                      isClearable
-                      value={this.state.selectedProject}
-                      onChange={this.handleProjectSelect}
-                      onInputChange={this.handleProjectCreate}
-                      options={this.state.existingProjects}
-                    />
-                  </div>
-                  <button type='submit' className='btn btn-primary'>Submit</button>
-                </form>
-              </div>
+         
+          <div style={{ display: (this.state.step === 0) ? 'block' : 'none' }}>
+            <div class='row'>
+              <CustomVisionProjectSelector
+                callback={this.customVisionProjectSelectCallback}
+                containerClass='step-container'
+                formSectionTitle='Step 1: Select or create a custom vision project'
+                formSectionBlurb='Choose an existing custom vision project, or type a name into the box to create a new custom vision project for your extracted image frames to be loaded into.' />
             </div>
           </div>
           
-          <div style={{ display: (!this.state.loading && this.state.step === 1) ? 'block' : 'none' }}>
+          <div style={{ display: (this.state.step === 1) ? 'block' : 'none' }}>
             <div class='row'>
               <VideoImageFrameExtractor
                 callback={this.extractVidoFramesCallback}
@@ -76,7 +49,7 @@ export class VideoFrameExtractor extends Component {
             </div>
           </div>
          
-          <div style={{ display: (!this.state.loading && this.state.step === 2) ? 'block' : 'none' }}>
+          <div style={{ display: (this.state.step === 2) ? 'block' : 'none' }}>
             <div className='row'>
               <VideoImageFramePublisher 
                 images={this.state.frames}
@@ -91,45 +64,8 @@ export class VideoFrameExtractor extends Component {
     );
   }
 
-  async getCustomVisionProjects() {
-    const response = await fetch('customvisionauthoring/listcvprojects');
-    const data = await response.json();
-
-    const cvProjects = data.map((p) => {
-      return {
-        value: p,
-        label: p
-      }
-    });
-
-    this.setState({ existingProjects: cvProjects });
-  }
-
-  handleProjectSelect = selectedProject => {
-    this.setState({ selectedProject });
-  };
-
-  async submitProject(e) {
-    e.preventDefault();
-    this.setState({ loading: true });
-
-    const requestBody = { projectName: this.state.selectedProject.value };
-
-    const response = await fetch('customvisionauthoring/createcvproject', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestBody)
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-      this.setState({ step: 1 });
-    }
-    this.setState({ loading: false });
+  customVisionProjectSelectCallback(result) {
+    this.setState({ step: 1 });
   }
 
   extractVidoFramesCallback(result) {
